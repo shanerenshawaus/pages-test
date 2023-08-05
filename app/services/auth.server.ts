629ca605ea80903f7c688cc1fec93bc4c69d8ccf
Shane Renshaw <5539363+shanerenshawaus@users.noreply.github.com>
2023-08-05 11:12:01 +1000

    Reviesions

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -1,43 +1,44 @@
 import { Authenticator } from "remix-auth";
 import { OAuth2Strategy } from "remix-auth-oauth2";
-import { loader, sessionStorage } from "~/services/session.server";
-
-// Create an instance of the authenticator, pass a generic with what
-// strategies will return and will store in the session
-export let authenticator = new Authenticator<User>(sessionStorage);
+import { getWorkersKVSessionStorage } from "~/services/session.server";
 
 
-authenticator.use(
-  new OAuth2Strategy(
-    {
-      authorizationURL: "https://provider.com/oauth2/authorize",
-      tokenURL: "https://provider.com/oauth2/token",
-      clientID: "asd",
-      clientSecret: "asd",
-      callbackURL: "https://example.app/auth/callback",
-      useBasicAuthenticationHeader: false, // defaults to false
-    },
-    async ({
-      accessToken,
-      refreshToken,
-      extraParams,
-      profile,
-      context,
-      request,
-    }) => {
-      // here you can use the params above to get the user and return it
-      // what you do inside this and how you find the user is up to you
-      return await getUser(
+export async function getAuthentication(env: Env) {
+  let authenticator = await new Authenticator<User>(await getWorkersKVSessionStorage(env));
+  authenticator.use(
+    new OAuth2Strategy(
+      {
+        authorizationURL: env.OAUTH_AUTH_URL,
+        tokenURL: env.OAUTH_TOKEN_URL,
+        clientID: env.OAUTH_CLIENT_ID,
+        clientSecret: env.OAUTH_CLIENT_SECRET,
+        callbackURL: env.OAUTH_CALLBACK_URL,
+        useBasicAuthenticationHeader: false, // defaults to false
+      },
+      async ({
         accessToken,
         refreshToken,
         extraParams,
         profile,
         context,
-        request
-      );
-    }
-  ),
-  // this is optional, but if you setup more than one OAuth2 instance you will
-  // need to set a custom name to each one
-  "user-pass"
-);
+        request,
+      }) => {
+        // here you can use the params above to get the user and return it
+        // what you do inside this and how you find the user is up to you
+        return await getUser(
+          accessToken,
+          refreshToken,
+          extraParams,
+          profile,
+          context,
+          request
+        );
+      }
+    ),
+    // this is optional, but if you setup more than one OAuth2 instance you will
+    // need to set a custom name to each one
+    "user-pass"
+  );
+  return authenticator
+
+}
--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -1,4 +1,4 @@
-import { createCookie, createWorkersKVSessionStorage } from "@remix-run/cloudflare";
+import { LoaderFunction, createCookie, createCookieSessionStorage, createWorkersKVSessionStorage, json,AppLoadContext } from "@remix-run/cloudflare";
 
 import { useLoaderData } from "@remix-run/react";
 import { useContext } from "react";
@@ -9,28 +9,11 @@ const sessionCookie = createCookie("__session", {
   sameSite: true,
 });
 
-
-export interface Env {
-  // If you set another name in wrangler.toml as the value for 'binding',
-  // replace "DB" with the variable name you defined.
-  SESSIONS_BINDING: KVNamespace;
-}
-
-
-// export the whole sessionStorage object
-// export let sessionStorage = createCookieSessionStorage({
-//   cookie: {
-//     name: "_session", // use any name you want here
-//     sameSite: "lax", // this helps with CSRF
-//     path: "/", // remember to add this so the cookie will work in all routes
-//     httpOnly: true, // for security reasons, make this cookie http only
-//     secrets: ["s3cr3t"], // replace this with an actual secret
-//   },
-// });
-
-const env = useContext():Env
-export let sessionStorage =createWorkersKVSessionStorage({
+export async function getWorkersKVSessionStorage(env) {
+  return createWorkersKVSessionStorage({
     // The KV Namespace where you want to store sessions
+    // This maps to the binding name set in wranlger.toml.
     kv: env.SESSIONS_BINDING,
     cookie: sessionCookie,
   });
+}

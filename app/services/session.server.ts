39ce55b68892c7c63328949e782e8a6e5612c816
Shane Renshaw <5539363+shanerenshawaus@users.noreply.github.com>
2023-08-05 11:18:08 +1000

    session conflict

--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -9,32 +9,8 @@ const sessionCookie = createCookie("__session", {
   sameSite: true,
 });
 
-<<<<<<< HEAD
 export async function getWorkersKVSessionStorage(env) {
   return createWorkersKVSessionStorage({
-=======
-
-export interface Env {
-  // If you set another name in wrangler.toml as the value for 'binding',
-  // replace "DB" with the variable name you defined.
-  SESSIONS_BINDING: KVNamespace;
-}
-
-
-// export the whole sessionStorage object
-// export let sessionStorage = createCookieSessionStorage({
-//   cookie: {
-//     name: "_session", // use any name you want here
-//     sameSite: "lax", // this helps with CSRF
-//     path: "/", // remember to add this so the cookie will work in all routes
-//     httpOnly: true, // for security reasons, make this cookie http only
-//     secrets: ["s3cr3t"], // replace this with an actual secret
-//   },
-// });
-
-const env = useContext()
-export let sessionStorage =createWorkersKVSessionStorage({
->>>>>>> d366bea15e2f5369bcb7641f2b9a6f34fe285cc7
     // The KV Namespace where you want to store sessions
     // This maps to the binding name set in wranlger.toml.
     kv: env.SESSIONS_BINDING,
